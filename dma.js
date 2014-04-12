ebebd35da0f09835487a37b6146bc0ef87a7bfd5
Felix De Montis <felix@demont.is>
2014-04-12 23:05:05 +0200

    adding a close button, issue #2

--- dma.js
+++ dma.js
@@ -20,18 +20,41 @@ xhr.onreadystatechange = function () {
     code.innerText = xhr.responseText;
 
     pre.appendChild(code);
+    
+    var closeBtn = document.createElement('button');
+    closeBtn.classList.add('closeBtn');
+    closeBtn.style.width  = '2.5em';
+    closeBtn.style.height = '2.5em';
+    closeBtn.style.position = 'absolute';
+    closeBtn.style.right = '1em';
+    closeBtn.style.top = '1em';
+    closeBtn.style.cursor = 'pointer';
+    closeBtn.style.fontSize = '1.5em';
+    closeBtn.style.margin = '0';
+    closeBtn.style.border = '0.5em solid';
+    closeBtn.style.borderRadius = '1.5em';
+    closeBtn.style.background = 'rgba(255, 255, 255, .5)';
+
+    closeBtn.innerText = '✖';
 
     document.firstElementChild.style.margin = '0';
     document.firstElementChild.style.width  = '100%';
     document.firstElementChild.style.height = '100%';
 
     document.firstElementChild.appendChild(pre);
+    document.firstElementChild.appendChild(closeBtn);
 
     document.firstElementChild.removeChild(document.querySelector('head'));
     document.firstElementChild.removeChild(document.querySelector('body'));
+    
+    closeBtn.addEventListener('click', reload);
 
   }
 
 };
 
 xhr.send();
+
+function reload () {
+  window.location.reload(true);
+}
